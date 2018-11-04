35df5c15394dd2fa433bfb56e8a0d2868791add0
designravenous <32999911+designravenous@users.noreply.github.com>
2018-11-04 17:32:56 +0100

    Update app_format.py

--- app_format.py
+++ app_format.py
@@ -1,5 +1,6 @@
 import datetime
 from flask import Flask, json
+import csv
 
 class format_class:
     def __init__(self,dictionary_list):
@@ -26,7 +27,18 @@ class format_class:
             outcome_list.append({"count":int(count_of_search)})
             with open('domains.json', 'w') as f:
                 json.dump(outcome_list, f, indent=2)
-
-        
         except:
             print("Error occured to convert list to JSON (except in function)")
+
+    def write_to_csv(self):
+        time_var = datetime.datetime.now()
+        current_date =str(time_var.year) + "_" + str(time_var.month) + "_" + str(time_var.day)
+        file_type = ".csv" 
+        count = len(self.dictionary_list)
+
+        with open('output_'+ current_date + file_type, 'w', newline='') as f:
+            csv_output = csv.writer(f)
+            csv_output.writerow(['Domain','http_response','date' ])
+            for item in self.dictionary_list:
+                csv_output.writerow([str(item['name']), str(item['http_response']), str(item['result_date'])])
+            csv_output.writerow(["Results: " + str(count) ])
