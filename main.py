b009eafea99ba854f7cd185dcc754eaed4889a97
designravenous <32999911+designravenous@users.noreply.github.com>
2018-11-05 18:39:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,16 +26,18 @@ date_string =str(time_var.year) + "-" + str(time_var.month) + "-" + str(time_var
 p1 = app.domain_list_handling(domains, date_string)
 result_http = p1.http_requests()
 
+
 #To call for the https methods
 #result_https = p1.https_requests()
 
 #Writing To TXT file
-file_action = app_format.format_class(result_http, date_string)
+file_action = app_format.format_class(result_http['Domains'], date_string)
 file_action.write_to_txt()
 
 #Writing to CSV file
 file_action.write_to_csv()
 
-#Writeing TO JSON
-file_action.write_to_json()
+#Writing TO JSON
+json_action = app_format.format_class(result_http, date_string)
+json_action.write_to_json()
 
