64f466cc78d940ae566a54b2cd2337e44457fd67
designravenous <32999911+designravenous@users.noreply.github.com>
2018-11-05 17:15:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import app
 import app_format
+import datetime
 
 def error_message():
     print("--- Script Error ---")
@@ -17,15 +18,19 @@ except:
 input_file = open(input_One,'r')
 domains = input_file.readlines()
 
+#Creating dateString
+time_var = datetime.datetime.now()
+date_string =str(time_var.year) + "-" + str(time_var.month) + "-" + str(time_var.day)
+
 #Calling method in class to do requests of the listitems
-p1 = app.domain_list_handling(domains)
+p1 = app.domain_list_handling(domains, date_string)
 result_http = p1.http_requests()
 
 #To call for the https methods
 #result_https = p1.https_requests()
 
 #Writing To TXT file
-file_action = app_format.format_class(result_http)
+file_action = app_format.format_class(result_http, date_string)
 file_action.write_to_txt()
 
 #Writing to CSV file
@@ -33,3 +38,4 @@ file_action.write_to_csv()
 
 #Writeing TO JSON
 file_action.write_to_json()
+
